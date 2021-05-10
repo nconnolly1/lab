faca36a3dab360a7c112818c72ec1937dafa5a34
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-05-10 13:39:35 +0100

    Use MAC address for DHCP

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ PKG=/tmp/vagrant_2.2.15_x86_64.deb
 curl -o "$PKG" https://releases.hashicorp.com/vagrant/2.2.15/vagrant_2.2.15_x86_64.deb
 sudo apt install "$PKG"
 rm -f "$PKG"
+
+vagrant plugin install vagrant-reload
