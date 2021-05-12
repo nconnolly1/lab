dbe836da01bd7592c5204f0d7f0f8e18edd75ef5
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-05-12 09:32:46 +0100

    Install vagrant 2.2.16

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PKG=/tmp/vagrant_2.2.15_x86_64.deb
-curl -o "$PKG" https://releases.hashicorp.com/vagrant/2.2.15/vagrant_2.2.15_x86_64.deb
+PKG=/tmp/vagrant_2.2.16_x86_64.deb
+curl -o "$PKG" https://releases.hashicorp.com/vagrant/2.2.16/vagrant_2.2.16_x86_64.deb
 sudo apt install "$PKG"
 rm -f "$PKG"
 
