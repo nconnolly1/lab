77c43b035dd8fb01f9ee4930602e0505ba9f9d49
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-05-27 15:20:35 +0100

    Comments for future work

--- env.bat
+++ env.bat
@@ -2,6 +2,9 @@
 
 set "PATH=%~dp0\scripts\ansible;%PATH%"
 
+rem Could select wsl dynamically based on result of
+rem wsl ansible --version >nul: 2>&1 && check %errorlevel%==0
+
 set ANSIBLE_ENV=cygwin
 set PACKER_CACHE_DIR=%USERPROFILE%\.packer\cache
 set VAGRANT_DISABLE_SMBMFSYMLINKS=1
