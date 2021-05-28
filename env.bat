23ec183b8319e9291ff89f940e53cdcfee0df457
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-05-28 22:32:20 +0100

    Update env scripts

--- env.bat
+++ env.bat
@@ -2,9 +2,9 @@
 
 set "PATH=%~dp0\scripts\ansible;%~dp0\scripts\packer;%PATH%"
 
-rem Could select wsl dynamically based on result of
-rem wsl ansible --version >nul: 2>&1 && check %errorlevel%==0
+set ANSIBLE_ENV=wsl
+wsl ansible --version >nul: 2>&1
+if not %errorlevel%==0 set ANSIBLE_ENV=cygwin
 
-set ANSIBLE_ENV=cygwin
 set PACKER_CACHE_DIR=%USERPROFILE%\.packer\cache
 set VAGRANT_DISABLE_SMBMFSYMLINKS=1
