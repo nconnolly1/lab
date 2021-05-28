610fa9ed04f03ef786c654357261d9cbd92c4e07
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-05-28 21:25:00 +0100

    Remove cygwin

--- scripts/install.bat
+++ scripts/install.bat
@@ -36,21 +36,6 @@ if errorlevel 1 (
 	if errorlevel 1 goto :eof
 )
 
-where /q cygwin
-if errorlevel 1 (
-	echo Installing Cygwin ...
-	choco install cygwin cyg-get -y -r
-	if errorlevel 1 goto :eof
-)
-
-set "CYGWIN=%SystemDrive%\tools\cygwin"
-
-if not exist "%CYGWIN%\usr\local\bin\ansible" (
-	echo Installing Cygwin Ansible ...
-	call cyg-get openssh python38 python38-pip python38-devel libssl-devel libffi-devel gcc-g++ python38-cryptography
-	"%CYGWIN%\bin\bash" --login -c "/usr/bin/python3.8.exe -m pip install wheel \"ansible==3.4.0\" pywinrm"
-)
-
 where /q sed
 if errorlevel 1 (
 	echo Installing sed ...
