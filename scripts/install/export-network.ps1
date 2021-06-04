060bf702fc00bdcb227c751762c27c97cf876131
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-06-04 15:26:42 +0100

    Use CRLF for config files

--- scripts/install/export-network.ps1
+++ scripts/install/export-network.ps1
@@ -1,7 +1,7 @@
 $dir = Split-Path $MyInvocation.MyCommand.Path -Parent
 $dir = $dir + '\config'
 
-$out = $dir + '\dhcp.conf'
+$out = $dir + '\conf.dhcp'
 Export-DhcpServer -File $out -Force
 
 $dns = $env:SystemRoot + '\System32\dns\lab.local.dns'
