ccfc9a3488f92eb538548c88e595a674b00a2d31
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2021-06-14 17:11:37 +0100

    Fetch yaml files for build

--- k8s/mayastor/install.sh
+++ k8s/mayastor/install.sh
@@ -2,31 +2,48 @@
 
 branch=${1:-develop}
 
+kube () {
+	if [ -f "/vagrant/mayastor/${branch}/$3" ]
+	then
+		echo kubectl $1 $2 "/vagrant/mayastor/${branch}/$3"
+		kubectl $1 $2 "/vagrant/mayastor/${branch}/$3"
+	elif [ -f "/vagrant/mayastor/yamls/$3" ]
+	then
+		echo kubectl $1 $2 "/vagrant/mayastor/yamls/$3"
+		kubectl $1 $2 "/vagrant/mayastor/yamls/$3"
+	elif [ "$3" = "mayastorpoolcrd.yaml" ]
+	then
+		echo kubectl $1 $2 "https://raw.githubusercontent.com/openebs/Mayastor/${branch}/csi/moac/crds/mayastorpool.yaml"
+		kubectl $1 $2 "https://raw.githubusercontent.com/openebs/Mayastor/${branch}/csi/moac/crds/mayastorpool.yaml"
+	else
+		echo kubectl $1 $2 "https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/$3"
+		kubectl $1 $2 "https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/$3"
+	fi
+}
+
+( cd /vagrant/mayastor; rm -rf yamls; bash ./fetch-yamls.sh "${branch}" )
+
 kubectl label node node-1 openebs.io/engine=mayastor
 kubectl label node node-2 openebs.io/engine=mayastor
 kubectl label node node-3 openebs.io/engine=mayastor
 
 kubectl create namespace mayastor
-kubectl create -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/moac-rbac.yaml
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/csi/moac/crds/mayastorpool.yaml
+kube create -f moac-rbac.yaml
+kube apply -f mayastorpoolcrd.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/nats-deployment.yaml
+kube apply -f nats-deployment.yaml
 kubectl -n mayastor get pods --selector=app=nats
 
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/csi-daemonset.yaml
+kube apply -f csi-daemonset.yaml
 kubectl -n mayastor get daemonset mayastor-csi
 
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/etcd/svc.yaml
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/etcd/svc-headless.yaml
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/etcd/statefulset.yaml
+kube apply -f etcd/svc.yaml
+kube apply -f etcd/svc-headless.yaml
+kube apply -f etcd/statefulset.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/moac-deployment.yaml
+kube apply -f moac-deployment.yaml
 kubectl get pods -n mayastor --selector=app=moac
-
-if [ -f /vagrant/mayastor/${branch}/mayastor-daemonset.yaml ]
-then kubectl apply -f /vagrant/mayastor/${branch}/mayastor-daemonset.yaml
-else kubectl apply -f https://raw.githubusercontent.com/openebs/Mayastor/${branch}/deploy/mayastor-daemonset.yaml
-fi
+kube apply -f mayastor-daemonset.yaml
 
 kubectl -n mayastor get daemonset mayastor
 
