28ab72cdcd4d9114669992eec0fa459a32e5b12f
Nick Connolly <47530082+datacore-nconnolly@users.noreply.github.com>
2022-03-03 23:08:49 +0000

    Only update interfaces if the file exists

--- packer/ubuntu/scripts/networking.sh
+++ packer/ubuntu/scripts/networking.sh
@@ -18,6 +18,8 @@ else
 fi
 
 # Disable Predictable Network Interface names and use eth0
-sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
+if [ -f /etc/network/interfaces ]; then
+    sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces
+fi
 sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
 update-grub;
